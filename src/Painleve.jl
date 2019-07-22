436b057d9833445b95b68faf470cbfd05634bd3c
mattarees <52246739+mattarees@users.noreply.github.com>
2019-07-22 16:28:31 +0100

    Update Painleve.jl

--- src/Painleve.jl
+++ src/Painleve.jl
@@ -1,10 +1,42 @@
 module Painleve
 
+using LinearAlgebra, RiemannHilbert, ApproxFun
+
 export painleve2
 
-function painleve2(s1,s2,s3,x)
+function painleve2(s1,s2,s3,x,n)
 	@assert s1 - s2 + s3 + s1*s2*s3 ≈ 0
-	error("Need to implement")
+
+    Γ = Segment(0, 2.5exp(im*π/6))   ∪
+    Segment(0, 2.5exp(im*π/2))   	 ∪
+    Segment(0, 2.5exp(5im*π/6))  	 ∪
+    Segment(0, 2.5exp(-5im*π/6)) 	 ∪
+    Segment(0, 2.5exp(-im*π/2))  	 ∪
+    Segment(0, 2.5exp(-im*π/6));
+
+    G = Fun( z -> if angle(z) ≈ π/6
+                    [1                             0;
+                     s1*exp(8im/3*z^3+2im*x*z)     1]
+                elseif angle(z) ≈ π/2
+                    [1                 s2*exp(-8im/3*z^3-2im*x*z);
+                     0                 1]
+                elseif angle(z) ≈ 5π/6
+                    [1                             0;
+                     s3*exp(8im/3*z^3+2im*x*z)     1]
+                elseif angle(z) ≈ -π/6
+                    [1                -s3*exp(-8im/3*z^3-2im*x*z);
+                     0                 1]
+                elseif angle(z) ≈ -π/2
+                    [1                             0;
+                     -s2*exp(8im/3*z^3+2im*x*z)    1]
+                elseif angle(z) ≈ -5π/6
+                    [1                -s1*exp(-8im/3*z^3-2im*x*z);
+                     0                 1]
+                end
+                    , Γ);
+
+    Φ = transpose(rhsolve(transpose(G), n));
+    return Φ
 end
 
 end # module
