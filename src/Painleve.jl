77ccfd285e33c4fb8b4c476a99505b20c5172af2
mattarees <52246739+mattarees@users.noreply.github.com>
2019-07-25 23:09:23 +0100

    Update Painleve.jl

--- src/Painleve.jl
+++ src/Painleve.jl
@@ -2,7 +2,7 @@ module Painleve
 
 using LinearAlgebra, RiemannHilbert, ApproxFun
 
-export painleve2_6ray, pl2def_no_s2_pos_x, pl2def_no_s2_neg_x1
+export painleve2_6ray, pl2def_no_s2_pos_x, pl2def_no_s2_neg_x1, pl2def_no_s2_neg_x2
 
 function painleve2_6ray((s1,s2,s3),x; n=600)
     @assert mod(n,6) == 0
@@ -111,6 +111,61 @@ function pl2def_no_s2_neg_x1((s1,s2,s3),x; n=50)
                      s3*exp(im*Θ(z))   1]
                 elseif angle(z+z_0) ≈ -3π/4
                     [1                  -s1*exp(-im*Θ(z));
+                     0                  1]
+                end
+                    , Γ);
+
+    Φ = transpose(rhsolve(transpose(G), n));
+    z = Fun(ℂ)
+    2(z*Φ[1,2])(Inf)
+end
+
+function pl2def_no_s2_neg_x2((s1,s2,s3),x; n=450)
+    @assert mod(n,9) == 0
+    @assert abs(s1 - s2 + s3 + s1*s2*s3) ≤ 100eps()
+    @assert x<0
+    @assert s2 == 0
+    
+    z_0 = sqrt(-x)/2
+
+    Γ = Segment(-z_0, z_0)                      ∪
+        Segment(z_0, z_0 + 2.5exp(im*π/4))      ∪
+        Segment(z_0 + 2.5exp(im*3π/4), z_0)     ∪
+        Segment(z_0 + 2.5exp(-im*3π/4), z_0)    ∪
+        Segment(z_0, z_0 + 2.5exp(-im*π/4))     ∪
+        Segment(-z_0, -z_0 + 2.5exp(im*π/4))    ∪
+        Segment(-z_0, -z_0 + 2.5exp(im*3π/4))   ∪
+        Segment(-z_0, -z_0 + 2.5exp(-im*3π/4))  ∪
+        Segment(-z_0, -z_0 + 2.5exp(-im*π/4))      
+    
+    Θ(z) = 8/3*z^3+2*x*z
+    
+    G = Fun( z -> if imag(z) ≈ 0
+                    [1-s1*s3            0;
+                     0                  1/(1-s1*s3)]
+                elseif angle(z-z_0) ≈ π/4
+                    [1                  0;
+                     s1*exp(im*Θ(z))    1]
+                elseif angle(z-z_0) ≈ 3π/4
+                    [1                            0;
+                     s1*exp(im*Θ(z))/(1-s1*s2)    1]
+                elseif angle(z-z_0) ≈ -3π/4
+                    [1                  -s3*exp(-im*Θ(z))/(1-s1*s2);
+                     0                  1]
+                elseif angle(z-z_0) ≈ -π/4
+                    [1                  -s3*exp(-im*Θ(z));
+                     0                  1]
+                elseif angle(z+z_0) ≈ π/4
+                     [1                            0;
+                     s1*exp(im*Θ(z))/(1-s1*s2)    1]
+                elseif angle(z+z_0) ≈ 3π/4
+                    [1                 0;
+                     s3*exp(im*Θ(z))   1]
+                elseif angle(z+z_0) ≈ -3π/4
+                    [1                  -s1*exp(-im*Θ(z));
+                     0                  1]
+                elseif angle(z+z_0) ≈ -π/4
+                    [1                  -s3*exp(-im*Θ(z))/(1-s1*s2);
                      0                  1]
                 end
                     , Γ);
