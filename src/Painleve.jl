ab6cd1e1b4699bb32b62be8c2d47aa25c9b6afe6
Sheehan Olver <solver@mac.com>
2019-07-26 11:11:52 +0100

    positive deformed test

--- src/Painleve.jl
+++ src/Painleve.jl
@@ -45,21 +45,21 @@ function painleve2deformed_no_s2((s1,s2,s3),x; n=800)
     @assert mod(n,4) == 0
     @assert abs(s1 - s2 + s3 + s1*s2*s3) ≤ 100eps()
 
-    Γ = Segment((im*x^(1/2))/2, 2.5exp(im*π/6)+(im*x^(1/2))/2)   ∪
-        Segment((im*x^(1/2))/2, 2.5exp(im*5π/6)+(im*x^(1/2))/2)      ∪
-        Segment((-im*x^(1/2))/2, 2.5exp(-im*5π/6)-(im*x^(1/2))/2)     ∪
-        Segment((-im*x^(1/2))/2, 2.5exp(-im*π/6)-(im*x^(1/2))/2)
+    Γ = Segment((im*x^(1/2))/2, 2.5+(im*x^(1/2))/2)   ∪
+        Segment((im*x^(1/2))/2, -2.5+(im*x^(1/2))/2)      ∪
+        Segment((-im*x^(1/2))/2, 2.5-(im*x^(1/2))/2)     ∪
+        Segment((-im*x^(1/2))/2, -2.5-(im*x^(1/2))/2)
 
-    G = Fun( z -> if angle(z-(im*x^(1/2))/2) ≈ π/6
+    G = Fun( z -> if angle(z-(im*x^(1/2))/2) ≈ 0
                     [1                             0;
                      s1*exp(8im/3*z^3+2im*x*z)     1]
-                elseif angle(z-(im*x^(1/2))/2) ≈ 5π/6
+                elseif angle(z-(im*x^(1/2))/2) ≈ π
                     [1                             0;
                      s3*exp(8im/3*z^3+2im*x*z)     1]
-                elseif angle(z+(im*x^(1/2))/2) ≈ -π/6
+                elseif angle(z+(im*x^(1/2))/2) ≈ 0
                     [1                -s3*exp(-8im/3*z^3-2im*x*z);
                      0                 1]
-                elseif angle(z+(im*x^(1/2))/2) ≈ -5π/6
+                elseif angle(z+(im*x^(1/2))/2) ≈ π
                     [1                -s1*exp(-8im/3*z^3-2im*x*z);
                      0                 1]
                 end
