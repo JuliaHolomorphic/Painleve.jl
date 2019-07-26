360a6d5d51acccc2580575a91e55dfe986b724ac
mattarees <52246739+mattarees@users.noreply.github.com>
2019-07-26 16:19:07 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Painleve
+using Painleve, Test
 
 @test painleve2_6ray((-im,0,im),0.0) ≈ 0.36706155154807807 # RHPackage
 @test painleve2_6ray((1,2,1/3),0.0) ≈ -0.5006840381943177 + 0.11748452477363962im # RHPackage
