3d45cb56c4c8073e9d8eb47fbdfcd9e2b55ffdd3
mattarees <52246739+mattarees@users.noreply.github.com>
2019-07-26 16:18:03 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using Painleve
-import Painleve: painleve2deformed_no_s2
 
 @test painleve2_6ray((-im,0,im),0.0) ≈ 0.36706155154807807 # RHPackage
 @test painleve2_6ray((1,2,1/3),0.0) ≈ -0.5006840381943177 + 0.11748452477363962im # RHPackage
