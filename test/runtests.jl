23e153f6cdcea2ff4fb67821cd588521072b6513
mattarees <52246739+mattarees@users.noreply.github.com>
2019-07-28 23:39:53 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,4 @@ using Painleve, Test
 @test pl2def_no_s2_pos_x((-im,0,im),1.0;n=400) ≈ painleve2_6ray((-im,0,im),1.0) ≈ 0.13564354350447155
 @test pl2def_no_s2_neg_x1((-im,0,im),-5.0) ≈ painleve2_6ray((-im,0,im),-5.0) ≈ 1.5794870908867027
 @test pl2def_no_s2_neg_x2((-2im,0,2im),-7.0) ≈ painleve2_6ray((-2im,0,2im),-7.0) ≈ 4.8192987279585155
+@test pl2def_no_s2_neg_x3((-2im,0,2im),-7.0) ≈ painleve2_6ray((-2im,0,2im),-7.0) ≈ 4.8192987279585155
