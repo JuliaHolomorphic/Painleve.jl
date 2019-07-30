3d3fef5e47cc2c4e84cec8d4d44f1d2291351ae7
Sheehan Olver <solver@mac.com>
2019-07-30 16:30:52 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,16 @@
 using Painleve, SingularIntegralEquations, RiemannHilbert, ApproxFun, Test
 import RiemannHilbert: RiemannDual, fpstieltjesmatrix!, orientedrightendpoint, Directed
 import ApproxFun: mobius
+
 @test painleve2_6ray((-im,0,im),0.0) ≈ 0.36706155154807807 # RHPackage
 @test painleve2_6ray((1,2,1/3),0.0) ≈ -0.5006840381943177 + 0.11748452477363962im # RHPackage
 
 @test pl2def_no_s2_pos_x((-im,0,im),1.0;n=400) ≈ painleve2_6ray((-im,0,im),1.0) ≈ 0.13564354350447155
 @test pl2def_no_s2_neg_x1((-im,0,im),-5.0) ≈ painleve2_6ray((-im,0,im),-5.0) ≈ 1.5794870908867027
+
+@test pl2def_no_s2_neg_x2((-0.5im,0,0.5im),-7.0) ≈ painleve2_6ray((-0.5im,0,0.5im),-7.0)
+
+
 @test pl2def_no_s2_neg_x2((-2im,0,2im),-7.0) ≈ painleve2_6ray((-2im,0,2im),-7.0) ≈ 4.819318173454774 # RHPackage
 @test pl2def_no_s2_neg_x3((-2im,0,2im),-7.0) ≈ painleve2_6ray((-2im,0,2im),-7.0) ≈ 4.819318173454774
 
